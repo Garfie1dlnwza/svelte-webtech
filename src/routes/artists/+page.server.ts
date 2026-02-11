1eba05c356d2e9ff725afdeea7dfef88a7ac2c98
G.gorfor <rawiponponsarutwanit@gmail.com>
2026-02-11 14:53:22 +0700

    fix : +page.server.ts for call api

--- src/routes/artists/+page.server.ts
+++ src/routes/artists/+page.server.ts
@@ -1,12 +1,10 @@
 import type { PageServerLoad } from './$types';
 import { apiClient } from '$lib/server/api-client';
 export const load: PageServerLoad = async () => {
-
 	const response = await apiClient.get('/api/artists');
-	console.log(response)
 	let artistsList = [];
-	if(response.status === 200){
-		artistsList = response.data;
+	if (response.status === 200) {
+		artistsList = response.data.data;
 	}
 
 	return {
